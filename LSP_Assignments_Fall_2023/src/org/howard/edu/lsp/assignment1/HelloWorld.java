69d073188e5c73ade416569324cc39fa6b6dec14
jDixon0004 <113641100+jDixon0004@users.noreply.github.com>
2023-08-24 19:58:05 -0400

    Initial commit.

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	/**
+	 * Jaelen Dixon
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
