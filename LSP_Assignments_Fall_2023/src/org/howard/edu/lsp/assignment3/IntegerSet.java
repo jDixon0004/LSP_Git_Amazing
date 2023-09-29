b7af389b282f727b843c718d62e7d5cf0a5d9bd4
jDixon0004 <113641100+jDixon0004@users.noreply.github.com>
2023-09-28 20:25:49 -0400

    Set up IntegerSet layout

--- LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment3/IntegerSet.java
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment3/IntegerSet.java
@@ -1,5 +1,71 @@
 package org.howard.edu.lsp.assignment3;
 
-public class IntegerSet {
+import java.util.ArrayList;
+import java.util.List;
 
+public class IntegerSet {
+	private List<Integer> set = new ArrayList<Integer>();
+	
+	public IntegerSet() {
+		
+	}
+	
+	public IntegerSet(ArrayList<Integer> set) {
+		this.set = set;
+	}
+	
+	public void clear() {
+		
+	}
+	
+	public int length() {
+		
+	}
+	
+	public boolean equals(Object b) {
+		if(b instanceof IntegerSet) {
+			
+		}
+		return false;
+	}
+	
+	public boolean contains(int value) {
+		
+	}
+	
+	public int largest() {
+		
+	}
+	
+	public int smallest() {
+		
+	}
+	
+	public void add(int item) {
+		
+	}
+	
+	public void remove(int item) {
+		
+	}
+	
+	public void union(IntegerSet intSetB) {
+		
+	}
+	
+	public void intersect(IntegerSet intSetB) {
+		
+	}
+	
+	public void complement(IntegerSet intSetB) {
+		
+	}
+	
+	boolean isEmpty() {
+		
+	}
+	
+	public String toString() {
+		
+	}
 }
