52709c1b044eeb1946614e4fbcdac909a4763f28
jDixon0004 <113641100+jDixon0004@users.noreply.github.com>
2023-10-15 21:51:30 -0400

    Added default constructor and javadocs

--- LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment4/IntegerSetException.java
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/assignment4/IntegerSetException.java
@@ -1,6 +1,18 @@
 package org.howard.edu.lsp.assignment4;
 
 public class IntegerSetException extends Exception {
+	
+	/**
+	 * Constructs an IntegerSetException with no detail message.
+	 */
+	public IntegerSetException() {
+		super();
+	}
+	
+	/**
+	 * Constructs an IntegerSetException with the specified detail message.
+	 * @param s the detail message
+	 */
 	public IntegerSetException(String s) {
 		super(s);
 	}
