db6fbb5bb1b237e32a9522cc8db03a028a68fcf0
jDixon0004 <113641100+jDixon0004@users.noreply.github.com>
2023-10-19 19:37:59 -0400

    Set up methods and attributes.

--- LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Book.java
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/problem1/Book.java
@@ -1,5 +1,21 @@
 package org.howard.edu.lsp.midterm.problem1;
 
 public class Book {
+	private String title;
+	private String author;
+	private int year;
 	
+	public Book(String title, String author, int year) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+	}
+	
+	public String toString() {
+		
+	}
+	
+	public boolean equals(Object o) {
+		
+	}
 }
