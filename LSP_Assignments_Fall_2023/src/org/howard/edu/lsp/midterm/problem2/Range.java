14dddfef2c53d377c9ce439c16eb7dc51415f73a
jDixon0004 <113641100+jDixon0004@users.noreply.github.com>
2023-10-19 19:57:28 -0400

    Initial implementation.

--- LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/problem2/Range.java
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/midterm/problem2/Range.java
@@ -1,5 +1,30 @@
 package org.howard.edu.lsp.midterm.problem2;
 
 public class Range {
-
+	private int start;
+	private int end;
+	
+	public Range(int start, int end) {
+		// *** NOTE: RANGE IS INCLUSIVE OF GIVEN VALUES (WAS NOT SPECIFIED IN INTRUCTIONS ***
+		// *** E.G.: Range(0,5) creates a range including 0,1,2,3,4, and 5 ***
+		this.start = start;
+		this.end = end;
+	}
+	
+	public boolean isValuesInRange(int value) {
+		return value >= this.start && value <= this.end;
+	}
+	
+	public boolean doRangesOverlap(Range otherRange) {
+		if(otherRange.start >= this.start && otherRange.start <= this.end)
+			return true;
+		else if(otherRange.end >= this.start && otherRange.end <= this.end)
+			return true;
+		
+		return false;
+	}
+	
+	public int size() {
+		return this.end - this.start + 1;
+	}
 }
