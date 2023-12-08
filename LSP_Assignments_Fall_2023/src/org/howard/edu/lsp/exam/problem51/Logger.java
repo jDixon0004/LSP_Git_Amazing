5a7c5eb16fb86faf4e3f06cc338b81afd132c3b7
jDixon0004 <113641100+jDixon0004@users.noreply.github.com>
2023-12-07 20:09:59 -0500

    Initial commit

---
+++ LSP_Assignments_Fall_2023/src/org/howard/edu/lsp/exam/problem51/Logger.java
@@ -0,0 +1,25 @@
+package org.howard.edu.lsp.exam.problem51;
+
+public class Logger {
+	private String log;
+	static Logger logger;
+	
+	private Logger() {
+		this.log = "";
+	}
+	
+	public void logMessage(String message) {
+		this.log += message + "\n";
+	}
+	
+	public String displayLog() {
+		return ("Log:\n" + log);
+	}
+	
+	public static Logger getInstance() {
+		if (logger == null) {
+			logger = new Logger();
+		}
+		return logger;
+	}
+}
